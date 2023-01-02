5e0eac9b05e6b941b4a3d73971123f4eb9feb1c9
Crisktina <crishidalgolopez@gmail.com>
2023-01-02 19:08:17 +0100

    printing numbers in calculator display

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-console.log("hello js");
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculator__keys");
+const display = document.querySelector(".calculator__display");
+
+keys.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
+
+    if (!action) {
+      console.log("number key!");
+    }
+
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      key.classList.add("is-depressed");
+      // Add custom attribute
+      calculator.dataset.previousKeyType = "operator";
+      console.log("operator key!");
+    }
+
+    if (action === "clear") {
+      console.log("clear key!");
+    }
+
+    if (action === "calculate") {
+      console.log("equal key!");
+    }
+
+    if (!action) {
+      if (displayedNum === "0") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+
+    if (action === "decimal") {
+      display.textContent = displayedNum + ".";
+    }
+
+    if (!action) {
+      if (displayedNum === "0" || previousKeyType === "operator") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+    // Remove .is-depressed class from all keys
+    Array.from(key.parentNode.children).forEach((k) =>
+      k.classList.remove("is-depressed")
+    );
+  }
+});
