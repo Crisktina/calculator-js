ebf9729f1976bdbf35416033cae5af3327752708
Crisktina <crishidalgolopez@gmail.com>
2023-01-03 17:22:27 +0100

    calculate the keys

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const calculate = (n1, operator, n2) => {
+  let result = "";
+
+  if (operator === "add") {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === "subtract") {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === "multiply") {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === "divide") {
+    result = parseFloat(n1) / parseFloat(n2);
+  }
+
+  return result;
+};
+
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator__keys");
 const display = document.querySelector(".calculator__display");
@@ -5,13 +21,43 @@ const display = document.querySelector(".calculator__display");
 keys.addEventListener("click", (e) => {
   if (e.target.matches("button")) {
     const key = e.target;
+    console.log(key);
     const action = key.dataset.action;
     const keyContent = key.textContent;
     const displayedNum = display.textContent;
     const previousKeyType = calculator.dataset.previousKeyType;
 
+    // Remove .is-depressed class from all keys
+    Array.from(key.parentNode.children).forEach((k) =>
+      k.classList.remove("is-depressed")
+    );
+
     if (!action) {
-      console.log("number key!");
+      // reemplamos la propiedad textContent de la pantalla para poner el numero que se ha hecho click
+      if (
+        displayedNum === "0" ||
+        previousKeyType === "operator" ||
+        previousKeyType === "calculate"
+      ) {
+        display.textContent = keyContent;
+      } else {
+        // concatenar mas numeros seguidos en caso que en el display no salga 0
+        display.textContent = displayedNum + keyContent;
+      }
+      calculator.dataset.previousKeyType = "number";
+    }
+
+    if (action === "decimal") {
+      if (!displayedNum.includes(".")) {
+        display.textContent = displayedNum + ".";
+      } else if (
+        previousKeyType === "operator" ||
+        previousKeyType === "calculate"
+      ) {
+        display.textContent = "0.";
+      }
+
+      calculator.dataset.previousKeyType = "decimal";
     }
 
     if (
@@ -20,42 +66,45 @@ keys.addEventListener("click", (e) => {
       action === "multiply" ||
       action === "divide"
     ) {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      // es suficiente firstValue y operator porque secondValue siempre existe
+      if (firstValue && operator) {
+        display.textContent = calculate(firstValue, operator, secondValue);
+      }
+
+      //añadir la clase is-depressed a la clave de operador.
       key.classList.add("is-depressed");
+
       // Add custom attribute
       calculator.dataset.previousKeyType = "operator";
-      console.log("operator key!");
+
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
     }
 
     if (action === "clear") {
-      console.log("clear key!");
+      if (key.textContent === "AC") {
+        calculator.dataset.firstValue = "";
+        calculator.dataset.modValue = "";
+        calculator.dataset.operator = "";
+        calculator.dataset.previousKeyType = "";
+      } else {
+        key.textContent = "AC";
+      }
+
+      display.textContent = 0;
+      calculator.dataset.previousKeyType = "clear";
     }
 
     if (action === "calculate") {
-      console.log("equal key!");
-    }
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
 
-    if (!action) {
-      if (displayedNum === "0") {
-        display.textContent = keyContent;
-      } else {
-        display.textContent = displayedNum + keyContent;
-      }
+      display.textContent = calculate(firstValue, operator, secondValue);
     }
-
-    if (action === "decimal") {
-      display.textContent = displayedNum + ".";
-    }
-
-    if (!action) {
-      if (displayedNum === "0" || previousKeyType === "operator") {
-        display.textContent = keyContent;
-      } else {
-        display.textContent = displayedNum + keyContent;
-      }
-    }
-    // Remove .is-depressed class from all keys
-    Array.from(key.parentNode.children).forEach((k) =>
-      k.classList.remove("is-depressed")
-    );
   }
 });
